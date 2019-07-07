4fd6db8b1433e1a1c13a4881710296868b7572f2
Jongho Kim <april9288@gmail.com>
2019-07-06 19:48:58 -0700

    new update on july

--- src/email.js
+++ src/email.js
@@ -1,26 +1,26 @@
 const sg = require('@sendgrid/mail')
 sg.setApiKey(process.env.SENDGRID_API_KEY)
 
-const lateEmail = (userEmail, userName, userSituation) => {
+const lateEmail = async (user, emailFrom, emailTo, situation) => {
 
-	let event
-	if (userSituation === 'accident') {
-		event = "a car accident"
-	} else if (userSituation === 'traffic') {
-		event = "stuck in traffic"
-	} else if (userSituation === 'broken') {
-		event = "my car broken"
-	} else if (userSituation === 'weather') {
-		event = "a bad situation"
+	let modifiedSituation
+	if (situation === "traffic") {
+		modifiedSituation = "stuck in heavy traffic"
+	} else if (situation === "accident") {
+		modifiedSituation = "into a car accident"
+	} else if (situation === "hurricane") {
+		modifiedSituation = "stuck in hurricane"
+	} else if (situation === "police") {
+		modifiedSituation = "pulled over by the police"
 	}
 
-	sg.send({
-		to: process.env.TARGET_EMAIL,
-		from: userEmail,
+	let sg_response = await sg.send({
+		to: emailTo,
+		from: emailFrom,
 		subject: "Sorry. I'm running late because...",
-		//text: `Deark Customer: \n \n Congratulations! You have been selected randomly by Broke-And-Need-Money to recieve a sum of $1,000,000! You will find the fund deposited into your primary checking account. Please thank James and Angela for signing you up for this amazing program that is exclusive to HRLA29. \n \n Sincerely, \n Consumer & Community Banking`
-		text: `Hello. This is ${userName}. I'm so sorry. I'm on my way in now, but I got ${event}. My apologies again. It shouldn’t be much longer. \n Thank you. \n ${userEmail} \n ${userName}` 
+		text: `Hello. This is ${user}. I'm so sorry. I'm on my way in now, but I got ${modifiedSituation}. My apologies again. It shouldn’t be much longer. \n Thank you. \n ${emailFrom} \n ${user}` 
 	})
+	return sg_response
 }
 
 module.exports = lateEmail
--- src/index.js
+++ src/index.js
@@ -1,51 +1,26 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const cors = require('cors')
-const axios = require('axios')
 const lateEmail = require('./email')
-const user = require('./userMock')
 
 const port = process.env.PORT
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
-	res.send("hello!")
-})
-
-app.post('/register', async (req,res)=> {
-	let handle = req.query.handle
-	let email = req.query.email
-
-	try{
-		let githubResponse = await axios({
-			method: 'GET',
-			url: `https://api.github.com/users/${handle}`,
-			headers: {"Accept": "application/vnd.github.v3+json"}
-		})
-
-		let user = {
-			name: githubResponse.data.name,
-			handle: handle,
-			profile: githubResponse.data.avatar_url,
-			email: email
-			}
-
-		res.status(200).send({status: 200, user})
-	}catch(e){
-		res.status(200).send({status: 400, user})
-	}
+	res.send("Hi. This is Running Late server!")
 })
 
 app.post('/email', async (req,res)=> {
-	let userEmail = req.body.chosenUser.email
-	let userName = req.body.chosenUser.name
-	let userSituation = req.body.situation
+	let { user, emailFrom, emailTo, situation } = req.body
 	try {
-		lateEmail(userEmail,userName,userSituation)
-		res.status(200).send({status: 200, message:"success"})
+		let sg_response = await lateEmail( user, emailFrom, emailTo, situation )
+		if (sg_response[0].statusCode === 202) {
+			res.status(200).send({status: sg_response[0].statusCode, message: "success"})
+		} else {
+			res.status(200).send({status: sg_response[0].statusCode, message:"fail"})
+		}
 	} catch(e) {
 		res.status(200).send({status: 400, message:"fail"})
 	}
